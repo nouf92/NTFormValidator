3da81ee154249ea6f6982c24b22edb679f0f4189
Nouf Alturaief <nouf@indielabs.sa>
2017-09-11 16:12:26 +0300

    init

---
+++ Example/Pods/Target Support Files/NTFormValidator/NTFormValidator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NTFormValidator : NSObject
+@end
+@implementation PodsDummy_NTFormValidator
+@end
---
+++ Example/Pods/Target Support Files/Pods-NTFormValidator_Tests/Pods-NTFormValidator_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NTFormValidator_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NTFormValidator_Tests
+@end
